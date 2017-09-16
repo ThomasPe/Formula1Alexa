692f9ab96a2f2cffb22e6f41372fd80350ff4e38
Thomas Pentenrieder <pentenrieder@outlook.com>
2017-09-16 17:24:47 +0200

    init

---
+++ Formula1Alexa/Skills/Formula1Skill.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Formula1Alexa.Skills
+{
+    public class Formula1Skill
+    {
+    }
+}
